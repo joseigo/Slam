b2591f64796625c48236608a47534ca1b3366eb3
joseEagle <55859473+joseigo@users.noreply.github.com>
2023-05-28 22:58:54 -0300

    Update testeqr.py

--- testeqr.py
+++ testeqr.py
@@ -2,17 +2,20 @@ import cv2
 
 from qr_code_ready import qrcode
 
-# imag = "http://192.168.4.1/capture"
-imag = 0
+imag = "http://192.168.4.1/capture"
+# imag = 0
 qq = qrcode()
 # print(qq.angulo)
 # print(qq.valor)
 # print(qq.distancia)
 # print(qq.pospxl)
 
+
 dec = int(input("Manda!"))
 while dec == 1:
-    b = qq.detectAnddecod(imag)
+    cap = cv2.VideoCapture(imag)
+    ret, frame = cap.read()
+    b = qq.detectAnddecod(frame, ret)
     # print("b:", b)
     if (b == 0):
         print("Qr code identificado mas não Decodificado")
