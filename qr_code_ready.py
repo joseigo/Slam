232cd71f76026c672d1e64461c4e54a4f153f71d
José Igo <55859473+joseigo@users.noreply.github.com>
2023-10-10 16:09:53 -0300

    Add files via upload

--- qr_code_ready.py
+++ qr_code_ready.py
@@ -1,6 +1,7 @@
 import math
 
 import cv2
+import numpy as np
 
 
 class qrcode:
@@ -11,21 +12,72 @@ class qrcode:
         self.angulo = -1
         self.distancia = -1
         self.qr_dict = {}
+        self.qr_width = 0.05
+        self.qr_height = 0.05
+        self.operation = ""
+
+    def setQrDimensions(self, width=0.05, height=0.05):
+        """Redimensiona o QR.
+            obs.: Medidas em m ."""
+        self.qr_height = width
+        self.qr_height = height
+
+    def setQrOperation(self, CodOp="RT_ID"):
+        """
+        Ajusta o tipo de retorno da função de Detecção e leitura
+        com base no Valor de CodOp (Código de Operação)
+         Podendo ser:
+        - CodOp = "RT_ID"       -> (r, theta, ID)
+        - CodOp = "RT_LABEL"    -> (r, theta, Label)
+        - CodOp = "RT_ID_LABEL" -> (r, theta, ID, Label)
+        - CodOp = "RT"          -> (r, theta)
+        - CodOp = "ID_LABEL"    -> (ID, Label)
+
+         ID = Código gerado "internamente" no Processamento
+
+         Label = Valor contido no QR.
+        """
+        self.operation = CodOp
+
+    def getQRReturn(self, qr_id):
+        """Obtem o devido retorno com base no OpCod definido na função setQrOperation"""
+        match self.operation:
+            case  "RT_ID":
+                return ([self.distancia, self.angulo, qr_id])
+            case "RT_LABEL":
+                return ([self.distancia, self.angulo, self.valor])
+            case "RT_ID_LABEL":
+                return ([self.distancia, self.angulo, qr_id, self.valor])
+            case "RT":
+                return ([self.distancia, self.angulo])
+            case "ID_LABEL":
+                return ([qr_id, self.valor])
+            case _:
+                return ([self.distancia, self.angulo, qr_id])
 
     def generate_qr_id(self):
-        # O ID é baseado no número de QRs já detectados
-        qr_id = len(self.qr_dict) + 1
-        return qr_id
+        """Gera um ID baseado no Nº de QRs já detectados"""
+        IdQr = len(self.qr_dict)
+        return IdQr
 
-    # detect and decod
-    def detectAnddecod(self, frame, ret):
+    # detect and decode
+
+    def detectAndDecode(self, frame, ret):
+
+        def AngleWrap(ang):
+            if ang > np.pi:
+                return ang - 2 * np.pi
+            elif ang < -np.pi:
+                return ang + 2 * np.pi
+            else:
+                return ang
 
         def obj_dist(h_im, res):
             fov = 65  # Campo de visão da camera (degrees)
-            w_real = 5  # Largura real do objeto (cm).
-            h_real = 5  # Altura real do objeto (cm).
+            w_real = self.qr_width  # Largura real do objeto (m).
+            h_real = self.qr_height  # Altura real do objeto (m).
             D_real = math.sqrt(w_real**2 + h_real**2)
-            dens = h_im/h_real  # Densidade de pixel por cm
+            dens = h_im/h_real  # Densidade de pixel por m
 
             # Calculo da distancia focal da camera
             d = (res[0]**2 + res[1]**2)**(1/2)
@@ -37,10 +89,10 @@ class qrcode:
             return Dist, dens
 
         def obj_ang(res, C, dist, dens):
+            d_Cam = 0.045  # Distância da camera ao centro do robô
             C_img = (res[0]/2, res[1]/2)  # obtendo o centro da imagem
-            d = abs(C[0] - C_img[0])
-            theta = math.atan(d/(dist*dens))
-            return math.degrees(theta)
+            d = (C[0] - C_img[0])
+            return math.atan2(d, ((dist+d_Cam)*dens))
 
         def img_processiment(frame):
             resol_i = frame.shape
@@ -113,7 +165,7 @@ class qrcode:
                             self.distancia = D
 
                             theta = obj_ang(res, C, D, dens_pixel)
-                            self.angulo = theta
+                            self.angulo = AngleWrap(theta)
 
                             frame = cv2.putText(
                                 frame, str(round(D, 2))+" cm", (C[0]-15, max(pnt[:, 1]).astype(int)+15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 255), 2)
@@ -135,8 +187,34 @@ class qrcode:
             else:
                 # Se leitura não for possivel, retorna '-1'
                 return (-1)
-
-            return ([self.distancia, self.angulo, qr_id])
+            return self.getQRReturn(qr_id)
         else:
             # Se leitura do arquivo não for possivel, retorna '-1'
             return (-1)
+
+
+def main():
+    # imag = "http://192.168.0.12/capture"
+    imag = 0
+    qq = qrcode()
+    qq.setQrDimensions(0.05, 0.05)
+    qq.setQrOperation("RT")
+
+    dec = int(input("Manda!"))
+    while dec == 1:
+        cap = cv2.VideoCapture(imag)
+        ret, frame = cap.read()
+        b = qq.detectAndDecode(frame, ret)
+        # print("b:", b)
+        if (b == 0):
+            print("Qr code identificado mas não Decodificado")
+        elif (b == -1):
+            print("Qr code não identificado!")
+        else:
+            print(b)
+            print(qq.qr_dict)
+        dec = int(input("Manda!"))
+
+
+if __name__ == "__main__":
+    main()
